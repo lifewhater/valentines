845e2e2accd7a9c87abb445adc90fb3aea37e32f
Prabhjot Singh <61862441+lifewhater@users.noreply.github.com>
2026-01-27 01:16:27 -0800

    Project setup complete. Now need to start working on the design and logic

---
+++ valentines/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class=" bg-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    Will You Be My Valentines?
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+:global(body) {
+  background-color: lab(23.44 22.9 -15.79);
+}
+</style>
