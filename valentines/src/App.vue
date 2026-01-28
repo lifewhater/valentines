cd272ed4195888f908b1ec2b22a6692bfe9a2dc8
Prabhjot Singh <61862441+lifewhater@users.noreply.github.com>
2026-01-28 15:31:56 -0800

    Revert "Added some styling and fixed the layout"
    This reverts commit ef1e0042fecc6d0b7b30bc54f0f7204839a3260f.

--- valentines/src/App.vue
+++ valentines/src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="flex-row mt-50 mx-5 space-y-10">
-    <div class="flex bg-purple-800 rounded-xl h-20 justify-center items-center shadow-lg shadow-purple-400/50">
-      <a class="text-white text-xl">Will You Be My Valentines?</a>
-    </div>
-    <div class=" flex justify-between text-white text-4xl gap-5">
-     <button class=" bg-gray-600 hover:bg-purple-400 border border-gray-500 py-2 px-5 rounded-xl w-full ">Yes</button>
-     <button class="bg-gray-600 hover:bg-purple-400 border border-gray-500  py-2 px-5 rounded-xl w-full ">No</button>
-    </div>
+  <div class=" bg-white rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    Will You Be My Valentines?
   </div>
-
 </template>
 
 <script setup lang="ts">
@@ -17,6 +10,6 @@
 
 <style scoped>
 :global(body) {
-  background-color: lab(0 0 0);
+  background-color: lab(23.44 22.9 -15.79);
 }
 </style>
